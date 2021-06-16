77f116406c2d2b81de26bf432621f64850d2a2d0
kripticia <65090462+kripticia@users.noreply.github.com>
2021-06-16 10:31:18 +0100

    Tweak from previous commit, now including test window on Swing

---
+++ src/main/graphics/SwingTestWindow.kt
@@ -0,0 +1,46 @@
+package main.graphics
+
+import main.data.GameMap
+import java.awt.*
+import javax.swing.*
+
+/*
+This file is simply a graphical way to test the GameMap generation.
+This is not intended to be the final graphical solution for the project.
+*/
+
+class SwingTestWindow (currentMap: GameMap, frame: JFrame = JFrame("Minesweeper"),
+                       private val panel:JPanel = JPanel()){
+
+    init {
+        frame.setSize(512, 512+24)
+        frame.layout = BorderLayout()
+        frame.contentPane.background = Color.BLACK
+        frame.isResizable = false
+        frame.isVisible = true
+        frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+
+        panel.layout = GridLayout(currentMap.width, currentMap.height)
+        frame.add(panel)
+        //val cellPanel: GridPanel =
+        createUICells(currentMap.map)
+        panel.revalidate()
+        panel.repaint()
+    }
+
+    private fun createUICells(map: Array<Array<String>>) {
+        val cells = Array(map.size){arrayOfNulls<JLabel>(map[0].size)}
+        for (i in map.indices) {
+            for (j in map[i].indices) {
+                val content = map[i][j]
+                cells[i][j] = JLabel(content)
+                cells[i][j]?.isOpaque = true
+                cells[i][j]?.background = Color.white
+                cells[i][j]?.border = BorderFactory.createLineBorder(Color.BLACK, 1)
+                cells[i][j]?.horizontalAlignment = SwingConstants.CENTER
+                cells[i][j]?.verticalAlignment = SwingConstants.CENTER
+                panel.add(cells[i][j])
+            }
+        }
+    }
+}
--- src/main/run.kt
+++ src/main/run.kt
@@ -1,5 +1,10 @@
 package main
 
-fun main() {
+import main.data.GameMap
+import main.graphics.SwingTestWindow
 
+fun main() {
+    val currentMap = GameMap()
+    currentMap.createNewMap(16,16,32)
+    SwingTestWindow(currentMap)
 }
